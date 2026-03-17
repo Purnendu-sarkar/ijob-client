5e36ef0d6c564426c7fa0b5980f6b7a82d548ade
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2026-03-18 00:09:19 +0600

    feat(dashboard): add protected pages for profile and change password

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,8 +1,10 @@
 import PublicFooter from "@/components/shared/PublicFooter";
+import PublicNavbar from "@/components/shared/PublicNavbar";
 
 const CommonLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
+      <PublicNavbar />
       {children}
       <PublicFooter />
     </>
--- src/components/shared/PublicNavbar.tsx
+++ src/components/shared/PublicNavbar.tsx
@@ -50,7 +50,7 @@ export default function PublicNavbar() {
 
   return (
     <nav className="sticky top-0 z-50 w-full border-b border-border bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
-      <div className="mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           {/* Logo */}
           <Link
